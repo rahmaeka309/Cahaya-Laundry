280012204bbdbbb8533e0c42355ee4342d88d2d4
rahmaeka309 <138545111+rahmaeka309@users.noreply.github.com>
2025-09-05 09:00:50 +0700

    Add files via upload

--- lib/koneksi.php
+++ lib/koneksi.php
@@ -1,1 +1,8 @@
-
+<?php
+	$conn=mysqli_connect('localhost','root','','dblaundry');
+	//if (!$conn) {
+		//die("koneksi gagal :" .mysqli_connect_error());
+	//}else{
+		//echo "koneksi berhasil";
+	//}
+?>
